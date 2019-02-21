aa9e5a604d8607915e7b31acc5ed5e8316498014
jurikri <41930943+jurikri@users.noreply.github.com>
2019-02-21 16:31:35 +0900

    Update file_import_spike.m

--- file_import_spike.m
+++ file_import_spike.m
@@ -1,11 +1,12 @@
-function msCell = file_import_spike(path2)
+function [msCell, msName] = file_import_spike(path2,s,e)
 ix = 1
 file2 = dir([char([path2]) '\*.*'])
 for i = 1:size(file2,1)
     try
        tmp = file2(i).name;
        filepath = [path2 tmp];
-       msCell{ix} = dlmread(filepath, ',', 13, 15);
+       msCell{ix} = dlmread(filepath, ',', s, e);
+       msName{ix} = [path2 tmp];
        ix = ix+1;
     catch
     end
